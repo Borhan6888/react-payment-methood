b40140404fd54a9b59b7cf3fdc456687234186da
BORHAN <borhanuddin6888@gmail.com>
2022-06-23 14:25:18 +0600

    react payment methood

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+  //Product Object name,price
+
+  // const configure()
+
+  const [product] = useState({
+    name: 'Sample Mario Game',
+    price: 1000,
+    description: 'This is sample mario game',
+  });
+
+  async function handleToken(token, adresses) {
+    const response = await axios.post('http://localhost:5000/checkout', {
+      token,
+      product,
+    });
+    console.log(response.status);
+    if (response.status === 200) {
+      toast('Success Pyment is Completed', { type: 'success' });
+    } else {
+      toast('Failure payment is not completed', { type: 'error' });
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br></br>
+      <div className="container">
+        <h1 className="text-center">Stripe Payment Checkout Demo</h1>
+        <br></br>
+
+        <h2 className="text-center">Product Info</h2>
+        <h2 className="text-center">Product Name: {product.name}</h2>
+        <h2 className="text-center">Product Price: {product.price}</h2>
+        <h2 className="text-center">
+          Product description: {product.description}
+        </h2>
+
+        <br></br>
+
+        <div className="form-group-container">
+          <StripeCheckout
+            className="center"
+            stripeKey="pk_test_TYooMQauvdEDq54NiTphI7jx"
+            token={handleToken}
+            amount={product.price * 100}
+            name={product.name}
+            billingAddress
+            shippingAddress
+          />
+        </div>
+      </div>
     </div>
   );
 }
